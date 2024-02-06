7a289fa4e49cfbe9908e516794b4ae3e59324c05
Stowy <stowy@protonmail.ch>
2024-02-06 14:54:20 +0100

    Added github actions and some modifications

--- astro.config.ts
+++ astro.config.ts
@@ -5,8 +5,7 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
-import { readFileSync } from "fs";
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
